96fa243411d61c56155d31c0ad351e9d90147891
Satyendra Singh Naruka <snaruka@qualys.com>
2019-11-03 00:33:07 +0530

    - Adding basic app with code splitting and a single container - Adding theme support using css variables

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const LazySearchPage = loadable(() => import('./containers/search/Search'));
+
+class App extends Component {
+  getRoutes = () => {
+    return (
+      <Router>
+        <Route exact path="/" component={LazySearchPage} />
+      </Router>
+    );
+  };
+
+  render() {
+    return (
+      <div className={styles['app']}>
+        <header className={styles['header']}>
+          <img src={logo} className={styles['logo']} alt="logo" />
+          <h1 className={styles['title']}>Search</h1>
+        </header>
+        <section className={styles['routes']}>{this.getRoutes()}</section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/containers/search/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './Search.module.css';
+
+class Search extends Component {
+
+  render() {
+    return (
+      <div className={styles['app']}>
+      </div>
+    );
+  }
+}
+
+export default Search;
